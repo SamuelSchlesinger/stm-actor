e0e71978cefe35b1c672eae2b7aab805d02d9e1b
Samuel Schlesinger <sgschlesinger@gmail.com>
2019-06-30 16:38:25 -0400

    Fixed up for a first version commit

--- example/Main.hs
+++ example/Main.hs
@@ -2,114 +2,12 @@
              ExistentialQuantification,
              FlexibleContexts,
              ScopedTypeVariables,
-             LambdaCase #-}
+             LambdaCase,
+             TypeOperators,
+             MultiParamTypeClasses #-}
 
 module Main where
 
 import Control.Concurrent.Actor.Prelude
-import System.Exit
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Control.Monad.State.Strict
-import System.Random hiding (next)
-import Control.Monad.Fail
-import System.Environment
 
-data Spec m env = forall msg a s. Spec {
-  actionT :: ActionT msg (StateT s m) a,
-  onFinish :: a -> m (),
-  initState :: env -> m s
-}
-
-data ActiveSpec m env = forall msg a s. ActiveSpec {
-  activeActionT :: ActionT msg (StateT s m) a,
-  activeOnFinish :: a -> m (),
-  activeInitState :: env -> m s,
-  activeAddress :: Address msg,
-  activeResult :: TMVar (Result a)
-}
-
-unactive :: ActiveSpec m env -> Spec m env
-unactive ActiveSpec{..} = Spec activeActionT activeOnFinish activeInitState
-
-data M = M {
-  tid :: ThreadId,
-  exc :: SomeException
-}
-
-type S m env = Map ThreadId (ActiveSpec m env)
-
-type Supervisor m env = ActionT M (StateT (S m env) m) ()
-
-supervisor0 :: (MonadBaseControl IO m, MonadFail m) => env -> Supervisor m env
-supervisor0 env = do
-  specs <- get
-  if Map.null specs then do
-    return ()
-  else do
-    m <- nextMaybe
-    case m of
-      Just M{..} -> do
-        specs <- get
-        case Map.lookup tid specs of
-          Nothing -> supervisor0 env
-          Just a -> do
-            put $ Map.delete tid specs
-            startSpec (unactive a) env
-            supervisor0 env
-      Nothing -> do
-        yield
-        forM_ (Map.toList specs) $ \(tid, ActiveSpec{..}) -> do
-          m <- atomicallyM $ tryReadTMVar activeResult
-          case m of
-            Just (Success x) -> do
-              put $ Map.delete tid specs
-            _ -> return ()
-        supervisor0 env
-
-startSpec :: (MonadFail m, MonadBaseControl IO m) => Spec m env -> env -> ActionT M (StateT (S m env) m) ()
-startSpec Spec{..} env = do
-  m <- get
-  s <- lift $ lift $ initState env
-  (address, result) <- lift . lift $ evalStateT (spawnM (yield >> actionT)) s
-  watch address M
-  let activeSpec = ActiveSpec
-                     actionT
-                     onFinish
-                     initState
-                     address
-                     result
-  lift $ lift $ forkFinally (do
-    ans <- atomicallyM $ readTMVar result
-    case ans of
-      Success a -> return a
-      Failure e -> error "Sad") (\case
-        Right a -> onFinish a
-        Left  e -> return ())
-  put $ Map.insert (addressThreadId address) activeSpec m
-
-supervise :: (MonadFail m, MonadBaseControl IO m) => [Spec m env] -> env -> m (Address M, TMVar (Result ()))
-supervise specs env = evalStateT (spawnM $ go specs env) Map.empty where
-  go [] env = supervisor0 env
-  go (spec : specs) env = do
-    startSpec spec env
-    go specs env
-
-badboy :: ActionT () (StateT () IO) (ThreadId, Integer)
-badboy = do
-  x :: Integer <- liftBase $ randomRIO (1, 10)
-  if x == 6 then do
-    id <- myThreadId
-    return (id, 0)
-  else
-    liftBase $ exitWith $ ExitFailure 1
-
-test :: Int -> [Spec IO ()]
-test n = take n $ repeat $ Spec badboy print (const $ return ())
-
-main = do
-  [nArg] <- getArgs
-  let n = read nArg
-  (address, result) <- supervise (test n) ()
-  atomically $ readTMVar result
-  return ()
+main = pure ()
--- src/Control/Concurrent/Actor.hs
+++ src/Control/Concurrent/Actor.hs
@@ -9,9 +9,11 @@
 
 module Control.Concurrent.Actor
     ( ActionT, Address, Result(..), LinkException(..), (:->)(..),
-      spawnM, spawnIO, spawn, link, watch, spawnLink, spawnWatch,
+      spawnM, spawnMFinally, spawnIO, spawnIOFinally, spawn,
+      link, watch, spawnLink, spawnWatch,
       next, nextMaybe, receive, send, flush,
-      myAddress, killAddress, throwToAddress, addressThreadId ) where
+      myAddress, killAddress, throwToAddress, addressThreadId, addressConvert,
+      remoteCatch, remoteOnSuccess, onSuccess ) where
 
 import Control.Concurrent.Lifted
 import Control.Concurrent.STM
@@ -19,19 +21,25 @@ import Control.Monad.Trans.Control
 import Control.Monad.Trans
 import Control.Exception.Lifted
 import Control.Monad.State.Class
+import Control.Monad.Reader.Class
 import Control.Monad.Base
-import Control.Monad
+import Control.Monad.Fail
+import Prelude hiding (fail)
+import Control.Monad hiding (fail)
 
 newtype ActionT msg m a = ActionT
   { unActionT :: TQueue msg
-             -> TMVar (SomeException -> IO ())
+             -> TMVar (Maybe SomeException -> IO ())
              -> m a }
 
 instance Functor f => Functor (ActionT msg f) where
   fmap f (ActionT act) = ActionT $ \mailbox onExit ->
     f <$> act mailbox onExit
 
-instance (Functor m, Monad m) => Applicative (ActionT msg m) where
+instance MonadFail m => MonadFail (ActionT msg m) where
+  fail x = lift (fail x)
+
+instance Monad m => Applicative (ActionT msg m) where
   pure x = ActionT $ \mailbox onExit -> pure x
   ActionT fact <*> ActionT xact = ActionT $ \mailbox onExit -> do
     f <- fact mailbox onExit
@@ -67,19 +75,26 @@ instance MonadBaseControl IO m => MonadBaseControl IO (ActionT msg m) where
   liftBaseWith = defaultLiftBaseWith
   restoreM = defaultRestoreM
 
-data LinkException e = LinkException ThreadId e
-  deriving (Show, Eq)
+data LinkException = LinkException ThreadId SomeException | LinkDone ThreadId
+  deriving (Show)
 
-instance Exception e => Exception (LinkException e)
+instance Exception LinkException
 
 instance MonadState s m => MonadState s (ActionT msg m) where
   get = lift get
   put a = lift (put a)
   state f = lift (state f)
 
+instance MonadReader r m => MonadReader r (ActionT msg m) where
+  ask = lift ask
+  local f x = do
+    x <- x
+    lift (local f (return x))
+  reader f = lift (reader f)
+
 data Address msg = forall msg'. Address {
   address :: !(TQueue msg'),
-  onExit :: !(TMVar (SomeException -> IO ())),
+  onExit :: !(TMVar (Maybe SomeException -> IO ())),
   threadId :: !ThreadId,
   encode :: !(msg -> msg')
 }
@@ -114,12 +129,32 @@ spawnM (ActionT act) = do
       f <- liftBase $ atomically $ do
         putTMVar result (Failure e)
         readTMVar onExit
-      liftBase $ f e
+      liftBase $ f (Just e)
     Right a -> do
-      liftBase $ atomically $ do
+      f <- liftBase $ atomically $ do
         putTMVar result (Success a)
-      return ()
+        readTMVar onExit
+      liftBase $ f Nothing
   return (Address address onExit threadId id, result)
+
+spawnMFinally :: MonadBaseControl IO m => ActionT msg m a -> (Maybe SomeException -> IO ()) -> m (Address msg, TMVar (Result a))
+spawnMFinally (ActionT act) slurp = do
+  address <- liftBase $ newTQueueIO
+  onExit <- liftBase $ newTMVarIO slurp
+  result <- liftBase $ newEmptyTMVarIO
+  threadId <-  forkFinally (act address onExit) $ \res -> case res of
+    Left e -> do
+      f <- liftBase $ atomically $ do
+        putTMVar result (Failure e)
+        readTMVar onExit
+      liftBase $ f (Just e)
+    Right a -> do
+      f <- liftBase $ atomically $ do
+        putTMVar result (Success a)
+        readTMVar onExit
+      liftBase $ f Nothing
+  return (Address address onExit threadId id, result)
+
 
 spawnIO :: ActionT msg IO a -> IO (Address msg, TMVar (Result a))
 spawnIO (ActionT act) = do
@@ -131,11 +166,30 @@ spawnIO (ActionT act) = do
       f <- atomically $ do
         putTMVar result (Failure e)
         readTMVar onExit
-      f e
+      f (Just e)
     Right a -> do
-      atomically $ do
+      f <- atomically $ do
         putTMVar result (Success a)
-      return ()
+        readTMVar onExit
+      f Nothing
+  return (Address address onExit threadId id, result)
+
+spawnIOFinally :: ActionT msg IO a -> (Maybe SomeException -> IO ()) -> IO (Address msg, TMVar (Result a))
+spawnIOFinally (ActionT act) slurp = do
+  address <- newTQueueIO
+  onExit <- newTMVarIO slurp
+  result <- newEmptyTMVarIO
+  threadId <-  forkFinally (act address onExit) $ \res -> case res of
+    Left e -> do
+      f <- atomically $ do
+        putTMVar result (Failure e)
+        readTMVar onExit
+      f (Just e)
+    Right a -> do
+      f <- atomically $ do
+        putTMVar result (Success a)
+        readTMVar onExit
+      f Nothing
   return (Address address onExit threadId id, result)
 
 spawn :: MonadBaseControl IO m => ActionT msg m a -> ActionT msg' m (Address msg, TMVar (Result a))
@@ -147,35 +201,67 @@ link Address{..} = ActionT $ \mailbox myOnExit -> do
   liftBase $ atomically $ do
     mine <- takeTMVar myOnExit
     theirs <- takeTMVar onExit
-    putTMVar myOnExit $ \e -> do
-      throwTo threadId (LinkException myThreadId e)
-      mine e
-    putTMVar onExit $ \e -> do
-      throwTo myThreadId (LinkException threadId e)
-      theirs e
+    putTMVar myOnExit $ \m -> case m of
+      Just e -> do
+        throwTo threadId (LinkException myThreadId e)
+        mine m
+      Nothing -> do
+        throwTo threadId (LinkDone myThreadId)
+        mine m
+    putTMVar onExit $ \m -> case m of
+      Just e -> do
+        throwTo myThreadId (LinkException threadId e)
+        theirs m
+      Nothing -> do
+        throwTo myThreadId (LinkDone threadId)
+        theirs m
 
-watch :: (MonadBase IO m, Exception e) => Address msg' -> (ThreadId -> e -> msg) -> ActionT msg m ()
+watch :: (MonadBase IO m, Exception e) => Address msg' -> (ThreadId -> Maybe e -> msg) -> ActionT msg m ()
 watch Address{..} enc = do
   me <- myAddress
   liftBase $ atomically $ do
     theirs <- takeTMVar onExit
-    putTMVar onExit $ \e -> do
-      tid <- myThreadId
-      case fromException e of
-        Just e_ -> send (enc tid e_) me >> theirs e
-        Nothing -> theirs e
+    putTMVar onExit $ \m -> case m of
+      Just e -> do
+        tid <- myThreadId
+        case fromException e of
+          Just e_ -> send (enc tid (Just e_)) me >> theirs m
+          Nothing -> theirs m
+      Nothing -> do
+        tid <- myThreadId
+        send (enc tid Nothing) me >> theirs m
 
 spawnLink :: MonadBaseControl IO m => ActionT msg m a -> ActionT msg' m (Address msg, TMVar (Result a))
 spawnLink actionT = do
-  (addr, result) <- spawn actionT
-  link addr
-  return (addr, result)
+  me <- myAddressBasic
+  let myId = addressThreadId me
+  (them, result) <- lift $ spawnMFinally actionT $ \m -> do
+    threadId <- myThreadId
+    case m of
+      Just e -> throwTo myId (LinkException threadId e)
+      Nothing -> throwTo myId (LinkDone threadId)
+  let theirId = addressThreadId them
+  mine <- liftBase $ atomically $ takeTMVar $ onExit me
+  liftBase $ atomically $ putTMVar (onExit me) $ \m -> case m of
+   Just e -> do
+     throwTo theirId (LinkException myId e)
+     mine m
+   Nothing -> do
+     throwTo theirId (LinkDone myId)
+     mine m
+  return (them, result)
 
-spawnWatch :: (Exception e, MonadBaseControl IO m) => ActionT msg' m a -> (ThreadId -> e -> msg) -> ActionT msg m (Address msg', TMVar (Result a))
+spawnWatch :: (Exception e, MonadBaseControl IO m) => ActionT msg' m a -> (ThreadId -> Maybe e -> msg) -> ActionT msg m (Address msg', TMVar (Result a))
 spawnWatch actionT enc = do
-  (addr, result) <- spawn actionT
-  watch addr enc
-  return (addr, result)
+  me <- myAddressBasic
+  let myId = addressThreadId me
+  (them, result) <- lift $ spawnMFinally actionT $ \m -> do
+    threadId <- myThreadId
+    case m of
+      Just e -> case fromException e of
+        Just e_ -> send (enc threadId (Just e_)) me
+      Nothing -> send (enc threadId Nothing) me
+  return (them, result)
 
 next :: MonadBase IO m => ActionT msg m msg
 next = ActionT $ \mailbox onExit -> do
@@ -184,7 +270,7 @@ next = ActionT $ \mailbox onExit -> do
 
 nextMaybe :: MonadBase IO m => ActionT msg m (Maybe msg)
 nextMaybe = ActionT $ \mailbox onExit -> do
-  msg <- liftBase . atomically $ tryReadTQueue mailbox
+  msg <- liftBase $ atomically $ tryReadTQueue mailbox
   return msg
 
 receive :: MonadBase IO m => (msg -> ActionT msg m a) -> ActionT msg m a
@@ -206,6 +292,11 @@ myAddress = ActionT $ \mailbox onExit -> do
   tid <- liftBase $ myThreadId
   return $ Address mailbox onExit tid convert
 
+myAddressBasic :: MonadBase IO m => ActionT msg m (Address msg)
+myAddressBasic = ActionT $ \mailbox onExit -> do
+  tid <- liftBase $ myThreadId
+  return $ Address mailbox onExit tid id
+
 send :: MonadBase IO m => msg -> Address msg -> m ()
 send msg Address{..} = liftBase $ atomically $ writeTQueue address (encode msg)
 
@@ -213,9 +304,35 @@ killAddress :: MonadBase IO m => Address msg -> ActionT msg' m ()
 killAddress Address{..} = ActionT $ \_ _ -> do
   killThread threadId
 
+addressConvert :: msg' :-> msg => Address msg -> Address msg'
+addressConvert (Address x y z f) = Address x y z (f . convert)
+
 addressThreadId :: Address msg -> ThreadId
 addressThreadId Address{..} = threadId
 
 throwToAddress :: (MonadBase IO m, Exception e) => Address msg -> e -> ActionT msg' m ()
 throwToAddress Address{..} e = ActionT $ \_ _ -> do
   throwTo threadId e
+
+remoteCatch :: (MonadBase IO m, Exception e) => Address msg -> (e -> IO ()) -> m ()
+remoteCatch Address{..} c = do
+  f <- liftBase $ atomically $ takeTMVar onExit
+  liftBase $ atomically $ putTMVar onExit $ \m -> case m of
+    Just e -> case fromException e of
+      Just e_ -> c e_ >> f m
+      Nothing -> f m
+    Nothing -> f m
+
+remoteOnSuccess :: MonadBase IO m => Address msg -> IO () -> m ()
+remoteOnSuccess Address{..} a = do
+  f <- liftBase $ atomically $ takeTMVar onExit
+  liftBase $ atomically $ putTMVar onExit $ \m -> case m of
+    Just _ -> f m
+    Nothing -> a >> f m
+
+onSuccess :: MonadBase IO m => IO () -> ActionT msg m ()
+onSuccess a = ActionT $ \_ onExit -> do
+  f <- liftBase $ atomically $ takeTMVar onExit
+  liftBase $ atomically $ putTMVar onExit $ \m -> case m of
+    Just _ -> f m
+    Nothing -> a >> f m
