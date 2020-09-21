61c9875c3871df812b296dc3eb18f60a8ebc368e
Samuel Schlesinger <samuel@simspace.com>
2020-09-20 23:43:34 -0400

    Fixed up haddocks and cabal file

--- src/Control/Concurrent/Actor.hs
+++ src/Control/Concurrent/Actor.hs
@@ -5,6 +5,15 @@
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE GADTs #-}
+{- |
+Module: Control.Concurrent.Actor
+Description: A basic actor model in Haskell
+Copyright: (c) Samuel Schlesinger 2020
+License: MIT
+Maintainer: sgschlesinger@gmail.com
+Stability: experimental
+Portability: POSIX, Windows
+-}
 module Control.Concurrent.Actor
 ( ActionT
 , Actor(..)
@@ -24,6 +33,7 @@ import Control.Exception
 import Data.Functor.Contravariant
 import Data.Queue
 
+-- | A type that contains the actions that 'Actor's will do.
 newtype ActionT message m a = ActionT
   { runActionT
     :: ActorContext message
@@ -57,8 +67,11 @@ data ActorContext message = forall a. ActorContext
   , messageQueue :: Queue message
   }
 
+-- | A handle to do things to actors, like sending them messages, fiddling
+-- with their threads, or adding an effect that will occur after they've
+-- finished executing.
 data Actor message = Actor
-  { addAfterEffect :: (Maybe SomeException -> IO ()) -> IO ()
+  { addAfterEffect :: (Maybe SomeException -> IO ()) -> STM ()
   , threadId :: ThreadId
   , send :: message -> STM ()
   }
@@ -66,6 +79,7 @@ data Actor message = Actor
 instance Contravariant Actor where
   contramap f (Actor addAfterEffect threadId send) = Actor addAfterEffect threadId (send . f)
 
+-- | Perform some 'ActionT' in a thread, with some cleanup afterwards.
 actFinally :: (Either SomeException a -> IO ()) -> ActionT message IO a -> IO (Actor message)
 actFinally errorHandler (ActionT act) = do
   onError <- atomically $ newTVar errorHandler
@@ -73,23 +87,28 @@ actFinally errorHandler (ActionT act) = do
   let ctx = ActorContext onError messageQueue
   threadId <- forkFinally (act ctx) (\result -> atomically (readTVar onError) >>= ($ result))
   pure $ Actor
-    (\afterEffect -> atomically $ modifyTVar onError (\f x -> f x <* afterEffect (leftToMaybe x)))
+    (\afterEffect -> modifyTVar onError (\f x -> f x <* afterEffect (leftToMaybe x)))
     threadId
     (enqueue messageQueue)
   where
     leftToMaybe (Left x) = Just x
     leftToMaybe _ = Nothing
 
+-- | Perform some 'ActionT' in a thread.
 act :: ActionT message IO a -> IO (Actor message)
 act = actFinally (const (pure ()))
  
+-- | Receive a message and do some 'ActionT' with it.
 receive :: MonadIO m => (message -> ActionT message m a) -> ActionT message m a
 receive f = ActionT \ctx -> do
   message <- liftIO $ atomically $ dequeue (messageQueue ctx)
   runActionT (f message) ctx
 
+-- | Receive a message and, in the same transaction, produce some result.
 receiveSTM :: MonadIO m => (message -> STM a) -> ActionT message m a
 receiveSTM f = ActionT \ctx -> liftIO (atomically (dequeue (messageQueue ctx) >>= f))
 
+-- | Use a natural transformation to transform an 'ActionT' on one base
+-- monad to another.
 hoistActionT :: (forall a. m a -> n a) -> ActionT message m a -> ActionT message n a
 hoistActionT f (ActionT act) = ActionT (fmap f act)
